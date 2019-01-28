6d8fa10d0cea65a3c36bfdd7c13005e1df38661a
mihika09 <mohta.mihika@gmail.com>
2019-01-28 09:01:08 +0530

    Echo server using asyncio

--- Server.py
+++ Server.py
@@ -1,122 +1,36 @@
-import socket
-
-HOST = ''
-PORT = 8888
-
-
-def get_handler(dct, req):
-
-	if len(req) > 0:
-		return "Message Body not expected"
-
-	if dct["Request_Target"] == '/':
-		dct["Request_Target"] += "index.html"
-
-	filepath = '/Users/mallikamohta/Desktop/Mihika/Static'+dct["Request_Target"]
-	print("filepath: ", filepath)
-	try:
-		file = open(filepath)
-		content = file.read()
-		file.close()
-
-	except FileNotFoundError:
-		content = "Page not found"
-
-	return content
+import asyncio
+host = '127.0.0.1'
+port = 8888
 
 
-def request_header_handler(req, dct):
+async def echo(reader, writer):
 
-	y = req.index('\r\n\r\n')
-
-	header = req[0:y]
-	header = header.split('\r\n')
-	print("\nHeader now: ", header)
-
-	req = req[y:]
-
-	if req[0:4] == '\r\n\r\n':
-		req = req[4:]
-		print("Request: ", req)
-		body_len = len(req)
-		print("Body_Len: ", body_len)
-
-	else:
-		print("Bad request")
-		return None
-
-	try:
-		for i in header:
-			# print("i: ", i)
-			if i:
-				key = i[0: i.index(':')]
-				value = i[i.index(':')+1:]
-				dct[key] = value
-
-	except:
-		print("Bad Request")
-		return None
-
-	if "Content-Length" not in dct:
-		conlen = 0
-
-	else:
-		conlen = dct["Content-Length"]
-
-	if conlen != body_len:
-		print("Bad request: expected no message body")
-		return None
-
-	return dct, req
+    header = await reader.readuntil(b"\r\n\r\n")
+    print("\n===header===\n", header)
+    response = "HTTP/1.1 200 OK\r\n"+'Content-Type: text/html\r\n\r\n'+"Hello World!"+'\r\n'
+    writer.write(response.encode())
+    await writer.drain()
+    writer.close()
 
 
-def request_handler(req):
+def execute_server():
 
-	dct = {}
-	x = req.index('\n')
+    loop = asyncio.get_event_loop()
+    coro = asyncio.start_server(echo, host, port)
+    server = loop.run_until_complete(coro)
 
-	try:
-		sl = req[0:x].split()
-		if len(sl) == 3:
-			dct["Method"] = sl[0]
-			dct["Request_Target"] = sl[1]
-			dct["Http_Version"] = sl[2]
-		else:
-			raise Exception
+    try:
+        loop.run_forever()
 
-	except:
-		print("Bad Request")
+    except KeyboardInterrupt:
+        print("Shutting down server")
 
-	req = req[x+1:]
-	res = request_header_handler(req, dct)
-	if res is not None:
-		dct = res[0]
-		req = res[1]
-
-	if dct["Method"] == 'GET':
-		get_res = get_handler(dct, req)
-		return get_res
+    finally:
+        server.close()
+        loop.run_until_complete(server.wait_closed())
+        loop.close()
 
 
 if __name__ == '__main__':
 
-	listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-	listen_socket.bind((HOST, PORT))
-	listen_socket.listen(5)
-	print("Listening on port: ", PORT)
-
-	try:
-		while True:
-			cliconn, cliaddr = listen_socket.accept()
-			request = cliconn.recv(1024).decode()
-			response = request_handler(request)
-			if response is not None:
-				http_response = "HTTP/1.1 200 OK\r\n"+'Content-Type: text/html\r\n\r\n'+response+'\r\n'
-				cliconn.send(http_response.encode())
-			else:
-				http_response = "HTTP/1.1 200 OK\r\n" + "Bad Request"
-			cliconn.close()
-
-	except KeyboardInterrupt:
-		print("Shutting down server")
+    execute_server()
